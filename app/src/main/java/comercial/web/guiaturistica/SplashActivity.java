4651029291e68eed6ec25dd64dd2aba445da49bf
Angel9817 <50686805+Angel9817@users.noreply.github.com>
2019-05-31 20:36:10 -0400

    Initial commit

---
+++ app/src/main/java/comercial/web/guiaturistica/SplashActivity.java
@@ -0,0 +1,30 @@
+package comercial.web.guiaturistica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    // Duración en milisegundos que se mostrará el splash
+    private final int DURACION_SPLASH = 250; // 3 segundos
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Tenemos una plantilla llamada splash.xml donde mostraremos la información que queramos (logotipo, etc.)
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
